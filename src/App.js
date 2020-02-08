c56697cbd4b60df88c2c704f10159b6ce056782f
Pat Cichocki <pat.cichocki.uk@gmail.com>
2020-02-08 17:22:57 +0000

    header component added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './Cssreset.css'
 import './App.css';
-import Navigation from './components/Navigation/Navigation'
+import Navigation from './components/Navigation/Navigation';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
+      <Header/>
     </div>
   );
 }
